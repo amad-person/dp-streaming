4dfb4649e9ed154a404ba1f48edca5d5899cc8ec
Aadyaa Maddi <aadyaa.maddi@gmail.com>
2023-10-09 23:24:27 -0400

    Refactor query engine

--- code/query.py
+++ code/query.py
@@ -7,6 +7,10 @@ class Query(ABC):
         pass
 
     @abstractmethod
+    def set_privacy_parameters(self, epsilon, delta=None):
+        pass
+
+    @abstractmethod
     def get_true_answer(self, *args):
         pass
 
@@ -16,27 +20,33 @@ class Query(ABC):
 
 
 class CountQuery(Query):
-    def __init__(self, epsilon, sensitivity):
+    def __init__(self, sensitivity=None, epsilon=None):
         super().__init__()
-        self.epsilon = epsilon
         self.sensitivity = sensitivity
+        self.epsilon = epsilon
+
+    def set_privacy_parameters(self, epsilon, delta=None):
+        self.epsilon = epsilon
 
     def get_true_answer(self, ids):
         return len(ids)
 
     def get_private_answer(self, ids):
         true_answer = self.get_true_answer(ids)
-        laplace_noise = np.random.laplace(loc=0, scale=(self.sensitivity/self.epsilon))
+        laplace_noise = np.random.laplace(loc=0, scale=(self.sensitivity / self.epsilon))
         return true_answer + laplace_noise
 
 
 class PredicateQuery(Query):
-    def __init__(self, dataset, predicate, epsilon, sensitivity):
+    def __init__(self, dataset=None, predicate=None, sensitivity=None, epsilon=None):
         super().__init__()
         self.dataset = dataset
         self.predicate = predicate
-        self.epsilon = epsilon
         self.sensitivity = sensitivity
+        self.epsilon = epsilon
+
+    def set_privacy_parameters(self, epsilon, delta=None):
+        self.epsilon = epsilon
 
     def get_true_answer(self, ids):
         df = self.dataset.select_rows_from_ids(ids)
@@ -44,5 +54,5 @@ class PredicateQuery(Query):
 
     def get_private_answer(self, ids):
         true_answer = self.get_true_answer(ids)
-        laplace_noise = np.random.laplace(loc=0, scale=(self.sensitivity/self.epsilon))
+        laplace_noise = np.random.laplace(loc=0, scale=(self.sensitivity / self.epsilon))
         return true_answer + laplace_noise
--- code/query_engine.py
+++ code/query_engine.py
@@ -1,8 +1,88 @@
+from abc import ABC, abstractmethod
 import pandas as pd
 from dataset import create_toy_dataset, create_fake_dataset, Dataset
 from query import CountQuery
 from node import NaiveNode
 import utils
+
+
+class QueryEngine(ABC):
+    def __init__(self):
+        pass
+
+    @abstractmethod
+    def run(self):
+        pass
+
+
+class NaiveBinaryQueryEngine(QueryEngine):
+    def __init__(self, dataset, query, epsilon, delta):
+        super().__init__()
+        self.dataset = dataset
+        self.query = query
+        self.epsilon = epsilon
+        self.delta = delta
+        self.naive_binary_insertions_map = {}
+        self.naive_binary_deletions_map = {}
+
+    def run(self):
+        true_answers, private_answers = [], []
+        num_nodes, current_tree_idx = 0, 0
+        for i, (ins_ids, del_ids) in enumerate(dataset.get_batches()):
+            node_i = i + 1
+
+            # start building new tree
+            if utils.get_tree_idx(node_i) != current_tree_idx:
+                num_nodes = 0
+                current_tree_idx = utils.get_tree_idx(node_i)
+
+            # build current nodes
+            self.query.set_privacy_parameters(epsilon=self.epsilon / utils.get_tree_height(node_i),
+                                              delta=self.delta)
+            ins_node = NaiveNode(ins_ids, self.query)
+            del_node = NaiveNode(del_ids, self.query)
+            num_nodes += 1
+
+            # add current nodes to maps
+            ins_tree_nodes = self.naive_binary_insertions_map.get(current_tree_idx, [])
+            del_tree_nodes = self.naive_binary_deletions_map.get(current_tree_idx, [])
+            ins_tree_nodes.append(ins_node)
+            del_tree_nodes.append(del_node)
+
+            # update maps by merging nodes
+            n = num_nodes
+            while n > 0:
+                if n % 2 == 0:
+                    # remove and merge last two nodes in the list
+                    second_ins_node = ins_tree_nodes.pop()  # ins_trees_node[-1]
+                    merged_ins_node = ins_tree_nodes.pop()  # ins_trees_node[-2]
+                    merged_ins_node.merge_node(second_ins_node)
+                    ins_tree_nodes.append(merged_ins_node)
+
+                    second_del_node = del_tree_nodes.pop()  # del_trees_node[-1]
+                    merged_del_node = del_tree_nodes.pop()  # del_trees_node[-2]
+                    merged_del_node.merge_node(second_del_node)
+                    del_tree_nodes.append(merged_del_node)
+                n = n / 2
+            self.naive_binary_insertions_map[current_tree_idx] = ins_tree_nodes
+            self.naive_binary_deletions_map[current_tree_idx] = del_tree_nodes
+
+            # combine answers from all trees
+            true_answer = 0
+            private_answer = 0
+            for tree_idx, tree_nodes in self.naive_binary_insertions_map.items():
+                for node in tree_nodes:
+                    true_answer += node.get_true_answer()
+                    private_answer += node.get_private_answer()
+            for tree_idx, tree_nodes in self.naive_binary_deletions_map.items():
+                for node in tree_nodes:
+                    true_answer -= node.get_true_answer()
+                    private_answer -= node.get_private_answer()
+
+            true_answers.append(true_answer)
+            private_answers.append(private_answer)
+
+        return true_answers, private_answers
 
 
 # Testing
@@ -26,68 +106,8 @@ if __name__ == "__main__":
                                      deletion_time_col="Deletion Time",
                                      time_interval=time_int)
     dataset.save_to_path("toy_dataset_batched_1day.csv")
-
+    query = CountQuery(sensitivity=1)
     epsilon = 10.0
-    true_answers, private_answers = [], []
-
-    naive_binary_insertions_map = {}
-    naive_binary_deletions_map = {}
-    num_nodes, current_tree_idx = 0, 0
-    for i, (ins_ids, del_ids) in enumerate(dataset.get_batches()):
-        node_i = i + 1
-        print(f"Batch: {node_i}")
-
-        # start building new tree
-        if utils.get_tree_idx(node_i) != current_tree_idx:
-            num_nodes = 0
-            current_tree_idx = utils.get_tree_idx(node_i)
-
-        # build current nodes
-        count_query = CountQuery(epsilon, sensitivity=utils.get_tree_height(node_i))
-        ins_node = NaiveNode(ins_ids, count_query)
-        del_node = NaiveNode(del_ids, count_query)
-        num_nodes += 1
-
-        # add current nodes to maps
-        ins_tree_nodes = naive_binary_insertions_map.get(current_tree_idx, [])
-        del_tree_nodes = naive_binary_deletions_map.get(current_tree_idx, [])
-        ins_tree_nodes.append(ins_node)
-        del_tree_nodes.append(del_node)
-
-        # update maps by merging nodes
-        n = num_nodes
-        while n > 0:
-            if n % 2 == 0:
-                # remove and merge last two nodes in the list
-                second_ins_node = ins_tree_nodes.pop()  # ins_trees_node[-1]
-                merged_ins_node = ins_tree_nodes.pop()  # ins_trees_node[-2]
-                merged_ins_node.merge_node(second_ins_node)
-                ins_tree_nodes.append(merged_ins_node)
-
-                second_del_node = del_tree_nodes.pop()  # del_trees_node[-1]
-                merged_del_node = del_tree_nodes.pop()  # del_trees_node[-2]
-                merged_del_node.merge_node(second_del_node)
-                del_tree_nodes.append(merged_del_node)
-            n = n / 2
-        naive_binary_insertions_map[current_tree_idx] = ins_tree_nodes
-        naive_binary_deletions_map[current_tree_idx] = del_tree_nodes
-        print(naive_binary_insertions_map)
-        print(naive_binary_deletions_map)
-
-        # combine answers from all trees
-        true_answer = 0
-        private_answer = 0
-        for tree_idx, tree_nodes in naive_binary_insertions_map.items():
-            for node in tree_nodes:
-                true_answer += node.get_true_answer()
-                private_answer += node.get_private_answer()
-        for tree_idx, tree_nodes in naive_binary_deletions_map.items():
-            for node in tree_nodes:
-                true_answer -= node.get_true_answer()
-                private_answer -= node.get_private_answer()
-
-        print(f"True Answer: {true_answer}")
-        print(f"Private Answer: {private_answer}")
-
-        true_answers.append(true_answer)
-        private_answers.append(private_answer)
+    delta = None
+    naive_binary_query_engine = NaiveBinaryQueryEngine(dataset, query, epsilon, delta)
+    true_ans, private_ans = naive_binary_query_engine.run()
